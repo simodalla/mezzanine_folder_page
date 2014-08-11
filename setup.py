000a476de7e555f1f987e2c90deac3af4a9b7789
Simone Dalla <simodalla@gmail.com>
2014-08-11 15:27:41 +0200

    Update for python3 install

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 import os
 from setuptools import setup, find_packages
 
-README = open('README.rst').read()
+README = open(os.path.join(
+    os.path.dirname(__file__), 'README.rst')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
